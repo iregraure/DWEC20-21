2a599d1a35115a33da992fa28c40813c71f4293d
iregraure <iregraure@gmail.com>
2020-09-17 16:54:25 +0200

    Añadido primer boletín de ejercicios, repaso de JavaScript

---
+++ 1. Ejercicio repaso JavaScript/Ejercicio2.js
@@ -0,0 +1,59 @@
+// Obtenemos los botones y el formulario
+let form = document.getElementById("formulario")
+let aniadir = document.getElementById("aniade")
+let borrar = document.getElementById("borra")
+
+// Añadimos el evento para el boton de añadir
+aniadir.addEventListener('click', function(e){
+    // Evitamos el comportamiento por defecto
+    e.preventDefault()
+    //Obtenemos los datos del formulario
+    let elemento = form.elemento.value
+    let posicion = form.posicion.value
+    if(posicion == ""){
+        alert("Debe introducir una posición")
+    }
+    else{
+        // Obtenemos la lista completa
+        let lista = document.getElementById("lista")
+        // Creamos el nodo que tenemos que añadir
+        let nuevoElemento = document.createElement("li")
+        nuevoElemento.appendChild(document.createTextNode(elemento))
+        // Obtenemos todos los elementos li de la lista
+        let elementos = lista.getElementsByTagName("li")
+        // Si la posición introducida es mayor que el número de elementos de la lista se añade el nuevo elemento al final
+        if(posicion > elementos.length){
+            lista.appendChild(nuevoElemento)
+        }
+        // Si no, se busca el elemento que está en la posición introducida y añadimos el nuevo elemento delante
+        else{
+            // Obtenemos todos los elementos li de la lista
+            let elementoPos = elementos[posicion-1]
+            // Añadimos el elemento delante del que acabamos de obtener
+            lista.insertBefore(nuevoElemento, elementoPos)
+        }
+    }
+})
+
+// Añadimos el evento para el botón de borrar
+borrar.addEventListener('click', function(e){
+    // Evitamos el comportamiento por defecto
+    e.preventDefault()
+    // Obtenemos la posición del formulario
+    let posicion = form.posicion.value
+    // Recuperamos la lista de elementos
+    let lista = document.getElementById("lista")
+    let elementos = lista.getElementsByTagName("li")
+    console.log(elementos)
+    // Si la posición introducida es mayor que los elementos que tiene la lista mostramos una alerta
+    if(posicion > elementos.length){
+        alert("No hay tantos elementos en la lista")
+    }
+    // Si no, eliminamos el elemento
+    else{
+        // Recuperamos el elemento que está en la posición introducida
+        let elemento = elementos[posicion-1]
+        // Eliminamos el elemento recuperado
+        lista.removeChild(elemento)
+    }
+})
---
+++ 1. Ejercicio repaso JavaScript/Ejercicio3.js
@@ -0,0 +1,38 @@
+// Creamos el array con las provincias
+let provincias = ["Sevilla", "Cadiz", "Huelva", "Malaga", "Granada", "Almeria", "Jaen", "Cordoba"]
+
+// Obtenemos el botón
+let ordena = document.getElementById("ordena")
+
+// Escribimos la etiqueta de apertura de table y fila
+document.write('<table border = "1"><tr>')
+// Recorremos los elementos ordenados
+provincias.forEach(element => {
+    // Abrimos la columna
+    document.write('<td>')
+    // Añadimos el elemento
+    document.write(element)
+    // Cerramos la columna
+    document.write('</td>')
+});
+// Cuando hemos terminado de añadir todas las filas cerramos la columna y la tabla
+document.write('</tr></table')
+
+// Añadimos el evento para el botón
+ordena.addEventListener('click', function(e){
+    // Ordenamos las provincias
+    provincias.sort()
+    // Escribimos la etiqueta de apertura de table
+    document.write('<table border = "1">')
+    // Recorremos los elementos ordenados
+    provincias.forEach(element => {
+        // Abrimos la fila y la columna
+        document.write('<tr><td>')
+        // Añadimos el elemento
+        document.write(element)
+        // Cerramos la columna y la fila
+        document.write('</td></tr>')
+    });
+    // Cuando hemos terminado de añadir todas las filas cerramos la tabla
+    document.write('</table')
+})
